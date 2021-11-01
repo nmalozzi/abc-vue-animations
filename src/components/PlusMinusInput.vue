649f49de7b76d6c907c56f4216c973ed433a15ff
Nick Malozzi <nmalozzi@gmail.com>
2021-11-01 12:03:07 -0600

    Adding a plus/minus input component

---
+++ src/components/PlusMinusInput.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="plus-minus-input">
+    <div class="plus-minus-button minus" v-on:click="mpminus()">
+      -
+    </div>
+    <div id="field">
+      <input type="number" v-model="newValue" disabled/>
+    </div>
+    <div class="plus-minus-button plus" v-on:click="mpplus()">
+      +
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: 0,
+      type: Number
+    },
+    min: {
+      default: 0,
+      type: Number
+    },
+    max: {
+      default: undefined,
+      type: Number
+    }
+  },
+  data() {
+    return {
+      newValue: 0
+    }
+  },
+  methods: {
+    mpplus: function () {
+      if (this.max === undefined || (this.newValue < this.max)) {
+        this.newValue = this.newValue + 1
+        this.$emit('input', this.newValue)
+      }
+    },
+    mpminus: function () {
+      if ((this.newValue) > this.min) {
+        this.newValue = this.newValue - 1
+        this.$emit('input', this.newValue)
+      }
+    }
+  },
+  watch: {
+    value: {
+      handler: function (newVal) {
+        this.newValue = newVal
+      }
+    }
+  },
+  created: function () {
+    this.newValue = this.value
+  }
+}
+</script>
+<style scoped>
+.plus-minus-input {
+  border: 1px solid silver;
+  border-radius: 5px;
+  background-color: #FFF;
+  margin: 0 5px 0 5px;
+  display: inline-block;
+  user-select: none;
+}
+
+.plus-minus-input div {
+  display: inline-block;
+}
+
+.plus-minus-input #field input {
+  width: 50px;
+  text-align: center;
+  font-size: 15px;
+  padding: 3px;
+  border: none;
+}
+
+.plus-minus-input .plus-minus-button {
+  padding: 3px 10px 3px 10px;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+.plus-minus-input .plus-minus-button:hover {
+  background-color: #DDD;
+}
+
+.plus-minus-input .plus-minus-button:active {
+  background-color: #c5c5c5;
+}
+</style>
--- src/layouts/WaterPressure.vue
+++ src/layouts/WaterPressure.vue
@@ -3,12 +3,14 @@
     <h1>{{ header }}</h1>
     <ToggleButton id="switch" />
     <SliderBar />
+    <PlusMinusInput />
   </div>
 </template>
 
 <script>
-import ToggleButton from '../components/ToggleButton.vue'
-import SliderBar from '../components/SliderBar.vue'
+import ToggleButton from '@/components/ToggleButton.vue'
+import SliderBar from '@/components/SliderBar.vue'
+import PlusMinusInput from "@/components/PlusMinusInput";
 
 export default {
   name: 'WaterPressureLayout',
@@ -16,6 +18,7 @@ export default {
     header: String
   },
   components: {
+    PlusMinusInput,
     ToggleButton,
     SliderBar
   }
