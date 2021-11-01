0e5f1a5b3354adc28e471e3825e57022424e37be
Nick Malozzi <nmalozzi@gmail.com>
2021-11-01 09:08:55 -0600

    Removing some more of the Vue setup boilerplate.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <WaterPressure msg="Welcome to Your Vue.js App"/>
-  </div>
+  <WaterPressure header="abc-vue-animations App"/>
 </template>
 
 <script>
 import WaterPressure from './layouts/WaterPressure.vue'
 
 export default {
-  name: 'App',
+  name: 'abc-vue-animations',
   components: {
     WaterPressure
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
